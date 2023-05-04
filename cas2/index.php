4bc91a1d9bcc8300185917ea73132c9f8125cf27
Dragica Maneva Atanasoski <116592591+dragicaatanasoski@users.noreply.github.com>
2023-05-04 19:00:56 +0200

    upade

---
+++ cas2/index.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>ova e naslov preku html</h1>
+   <h1> <?php 
+   $text = "Ova e naslov na vest";
+   $y = 30;
+   $x = 5;
+   $zbir = $x + $y;
+   $t = 101.5;
+   echo $text;  
+
+
+?> </h1>
+
+<?php 
+
+
+// single line comment
+
+#
+
+/*
+ahggdkghd
+*/
+
+//VARIABLES 
+
+?>
+<?php echo "<h1>ova e naslov preku html</h1>"; ?>
+<p> <?php echo $x + $y ?>e isto sto i <?php echo $zbir; ?></p>
+
+<h1><?php echo "<h1>". $text . "</h1>" ?>
+
+</body>
+
+</html>
+
+
+
+
