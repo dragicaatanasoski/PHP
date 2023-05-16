0d0d7cc09e0aed1f4c2e346ffbc196b379de0ac3
Dragica Maneva Atanasoski <116592591+dragicaatanasoski@users.noreply.github.com>
2023-05-16 19:09:49 +0200

    cas4

---
+++ cas4/index.php
@@ -0,0 +1,171 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cas 4</title>
+</head>
+<body>
+<?php 
+
+// $zbor= 'emilija';
+// $recenica= 'moze da se zeme emilija';
+// $savestrpos= strpos($recenica , $zbor);
+// echo $savestrpos;
+
+$ime= 'Dragica';
+echo strrev($ime);
+
+
+
+// echo 4+4;
+
+
+// $x= 6;
+// $y= 5;
+
+// if($x < $y ){
+//     echo 'X e pomalo od Y';
+// }  else{
+//     echo 'X e pogolemo od Y';
+// }
+
+// 
+
+
+// $varb='tri';
+// $varc= 'cetiri';
+
+// $broj_na_bukvi = strlen($varb);
+// echo $broj_na_bukvi;
+// definirajte cetri drugi promenlivi
+
+// echo $vara + $varb;
+
+
+?>
+
+<?php
+// echo "</br>";
+// echo "</br>";
+// echo "</br>";
+// $c=5;
+// echo ++$c;
+
+// echo $c;
+// echo $c++;
+// echo $c;
+// $c=5;
+// echo --$c;
+// echo $c;
+// echo $c--;
+// echo $c;
+?>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<!-- PHP Operators 
+ Aritmetic 
+ 
+ +
+ -
+ *
+ /
+ %
+ **
+
+ echo 4+4;
+ $x=4;
+ $y=4;
+ echo 
+
+
+ Logical Operator
+
+ and &&
+ or II 
+ not !
+
+ Comparation Operators
+
+ == equal
+ === identical
+ != not equal
+ <> not equal
+ !=== not identical
+ > pogolemo
+ < pomalo
+ >= pogolemo i ednakvo
+ <= pomalo i ednakvo
+
+ Assignment operators
+
+x = y 
+x += y -> x = x+y
+x -= y -> x = x-y
+x *= y -> x = x*y
+x /= y -> x = x/y
+x %= y -> x = x%y
+
+Incremet / Decrement
+
+++$c => $c = $c + (Increments $c by 1, returns $c)
+$c++ => $c = c+ 1 (returns $c,increment $c by 1)
+
+
+String
+
+.  concatanation
+.=
+
+
++Union
+
+== equality
+=== identical same type
+!= not equal
+!=== not identical
+-->
+
+
+</body>
+
+</html>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
