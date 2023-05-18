092a8401d848ae237ee3a2dbfe440a9f421317c2
Dragica Maneva Atanasoski <116592591+dragicaatanasoski@users.noreply.github.com>
2023-05-18 19:08:28 +0200

    cas5

---
+++ cas5/index.php
@@ -0,0 +1,231 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cas 5</title>
+</head>
+<body>
+
+
+<?php
+
+// $recenica= "Maja ima velosiped";
+// if (str_contains($recenica, 'velosiped')) {
+//     echo "Maja ima velosiped";
+// }
+// else {
+//     echo 'Elena ima velosiped';
+// }
+
+
+// $ime = 'Dragica';
+// if (strlen($ime)> 5){
+//  echo 'Imeto '. $ime .' ima poveke od pet karakteri';
+// }
+// elseif(strlen($ime)== 5){
+//     echo 'Imeto' . $ime .'ima tocno pet karakteri';
+// }
+   
+// else (strlen ($ime) < 5){
+//     echo 'Imeto'. $ime .'ime pomalku od pet karakteri';
+// }
+
+// $brzina = '70';
+// if ($brzina < 60){
+//     echo 'Bravo vozite so dozvolena brzina';
+// }
+// elseif($brzina > 60 && $brzina < 100){
+//     echo 'Ve molime namalete';
+// }
+// else {
+//     echo 'Life sentence';
+// }
+
+// $i =5;
+// switch($i){
+//     case 0:
+//         echo "i equals 0";
+//         break;
+//         case 1:
+//             echo "i equals 1";
+//             break;
+//             case 2:
+//             echo "i equals 2";
+//             default :
+//             echo "not defined";
+// }
+
+
+// $date = date("l");
+// echo "Денес е " . $date;
+
+// $date = date("l");
+
+// switch($date){
+//     case "Thursday":
+//         echo "Денес е четврток";
+//         break;
+//         case "Friday":
+//             echo "Денес е петок";
+//             break;
+//             case "Saturday":
+//             echo "Денес е сабота";
+
+            
+//             default :
+//             echo "not defined";
+// }
+
+
+// echo date ('H')
+
+$time = date ("H");
+if ($time > )
+
+?>
+
+<!-- ako saatot e povekje od 18 do 6 sabajle isprintaj dobro vecer ako e 6 sabajle i 11 isprintaj dobro utro ako e pomegju 11 i 18 dobar den -->
+
+
+
+
+<!-- Ako brojot na karakteri na Imeto Dime e pogolemo od 5 karakteri isprintajte deka dime ima poveke od pet karakteri ako ne dime im a pomalku od pet karakteri -->
+
+
+<!-- brzo vozime ako vozime popmalku od 60 da se isprinta bravo vozete 
+so so dozvolena brzina a ako vozime pomegju 60 i 100 ve molime namalete 
+ja vasata brzina ako vozite nad 100 zatvor -->
+
+
+
+
+
+
+
+
+
+
+<!-- PHP Operators 
+ Aritmetic 
+ 
+ +
+ -
+ *
+ /
+ %
+ **
+
+ echo 4+4;
+ $x=4;
+ $y=4;
+ echo 
+
+
+ Logical Operator
+
+ and &&
+ or II 
+ not !
+
+ Comparation Operators
+
+ == equal
+ === identical
+ != not equal
+ <> not equal
+ !=== not identical
+ > pogolemo
+ < pomalo
+ >= pogolemo i ednakvo
+ <= pomalo i ednakvo
+
+ Assignment operators
+
+x = y 
+x += y -> x = x+y
+x -= y -> x = x-y
+x *= y -> x = x*y
+x /= y -> x = x/y
+x %= y -> x = x%y
+
+Incremet / Decrement
+
+++$c => $c = $c + (Increments $c by 1, returns $c)
+$c++ => $c = c+ 1 (returns $c,increment $c by 1)
+
+
+String
+
+.  concatanation
+.=
+
+
++Union
+
+== equality
+=== identical same type
+!= not equal
+!=== not identical
+
+Conditiona statements
+
+
+if- 
+if(condition){
+
+}
+
+
+if-else
+if(condition){
+
+}
+else{
+
+}
+
+switch- 
+
+switch($i){
+    case 0:
+        echo "i equals 0";
+        break;
+        case 1:
+            echo "i equals 1";
+            break;
+            case 2:
+            echo "i equals 2";
+            deafult :
+            echo "not defined";
+}
+
+-->
+
+
+</body>
+
+</html>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
